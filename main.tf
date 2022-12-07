173f58aa3ad3ce5a21bfa7eea9ca2605d2c88ceb
SweetyDeena <117965542+SweetyDeena@users.noreply.github.com>
2022-12-07 19:49:25 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.34.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "newTfWebApp" {
+  name     = "MainWeb"
+  location = "West Europe"
+}
+
+resource "azurerm_service_plan" "appwebS001" {
+  name                = "appwebServ001"
+  resource_group_name = azurerm_resource_group.newTfWebApp.name
+  location            = azurerm_resource_group.newTfWebApp.location
+  sku_name            = "P1v2"
+  os_type             = "Windows"
+}
+
+resource "azurerm_windows_web_app" "winWebapp001" {
+  name                = "windwebApp001"
+  resource_group_name = azurerm_resource_group.newTfWebApp.name
+  location            = azurerm_service_plan.appwebS001.location
+  service_plan_id     = azurerm_service_plan.appwebS001.id
+
+  site_config {}
+}
